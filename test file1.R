8b1556eef152b24169012a451f290342368715c9
Fei Wang <wf199209@gmail.com>
2025-03-29 00:50:28 -0400

    first features

---
+++ test file1.R
@@ -0,0 +1,8 @@
+print("I love Python!")
+print("Python programming is fun!")
+print("I love programming in Python!")
+print("new features!")
+# This is a comment
+# why I cannot comment out the print statement?df
+# this is a comment
+# this is a new commment
